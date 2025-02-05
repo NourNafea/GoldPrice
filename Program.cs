a1f65c057979ef54215975c2e668e29775e127fb
NourNafea <nour68.nafea@gmail.com>
2025-02-05 19:48:55 +0200

    remove comments

--- Program.cs
+++ Program.cs
@@ -7,16 +7,7 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Add SendGrid configuration
 builder.Services.AddHttpClient();
 builder.Services.Configure<SendGridOptions>(builder.Configuration.GetSection("SendGrid"));
-
-// Add Durable Functions
-// Functionsbuilder.Services.AddDurableTaskClient();
-
-// Application Insights for monitoring (recommended for production)
-// builder.Services
-//     // .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
